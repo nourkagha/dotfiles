ffeef06b1b2cce59ee960e77257bf919a7a3be14
Nour Agha <50773468+nourkagha@users.noreply.github.com>
2021-05-01 16:49:15 +0300

    Update links

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,6 +1,4 @@
 # My custom aliases and functions
-alias ajour='/home/nour/Applications/ajour.AppImage'
-alias spicetify='/home/nour/spicetify-cli/spicetify'
 
 # Full update and cleanup
 function noups() {
